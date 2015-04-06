71d65c17752073a7e0dd699f50036f69a23b093f
Alyushin Vladislav <vladisl-a@yandex.ru>
2015-04-06 11:42:07 +0300

    Scripts
    My scripts

---
+++ InstallADDSRole.ps1
@@ -0,0 +1,40 @@
+﻿###########################################################
+# AUTHOR  : Alyushin Vladislav 
+# DATE    : 22-02-2015 
+# COMMENT : This script install Active Directory role,
+# VERSION : 1.0
+###########################################################
+# ERROR REPORTING ALL
+Set-StrictMode -Version latest
+#Import PowerShell Module 
+Try
+{
+Import-Module -Name ServerManager -ErrorAction
+}
+Catch
+{
+    Write-Host "[ERROR] Module couldn't be loaded. Script will stop!"
+    Exit 1
+}
+#Input and Set hostname for DC machine
+$hostname = Read-Host "Enter computer name"
+Rename-Computer -ComputerName $hostname -Verbose 
+#Install Windows Backup and SNMP Features
+try
+{
+Install-WindowsFeature SNMP-Service, SNMP-WMI-Provider, Windows-Server-Backup -Verbose -Confirm
+}
+Catch
+{
+    Write-Host "[ERROR] Couldn't be install feature. Script will stop!"
+}
+#Input Domain Name
+$dcname = Read-Host "Enter Domain Name"
+#Input Password for Safe Mode AD 
+$safepass = Read-Host "Enter Safe Mode Administrator Password"
+#Path for log file
+$log = Read-Host "Enter path for log file" + "\install.log"
+#Install AD-DS role
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Verbose
+#Create forest and install ad dc,dns
+Install-ADDSForest -domainname $dcname -CreateDnsDelegation -DomainMode Win2012R2 -ForestMode Win2012R2 -SafeModeAdministratorPassword $safepass -LogPath $log -Confirm -Verbose
---
+++ RemoteAccessExchange2013.ps1
@@ -0,0 +1,8 @@
+﻿#Скрипт просит ввести имя сервера к которому необходимо подключится
+$ExchangeServer = Read-Host "Введите полное имя сервера"
+#Запрашиваем учетные данные пользователя, под которым подключимся к серверу
+$UserCredential = Get-Credential
+#Создаем сессию подключения
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/PowerShell/ -Authentication Kerberos -Credential $UserCredential
+#Выполняем созданную сессию
+Import-PSSession $Session
